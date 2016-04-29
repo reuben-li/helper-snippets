4f4141183c4c035a3d35da60072a4b908f948d2e
Reuben Li <mesolithic@gmail.com>
2016-04-29 10:12:01 +0900

    Update performance.js

--- mongo/performance.js
+++ mongo/performance.js
@@ -9,3 +9,8 @@ db.col.createIndex({field:1,field2:-1}); db.col.createIndex({field:1,field2:1});
 
 // arrays and array objects
 db.col.createIndex({field.property:1})
+
+// see how mongo decides which logic to use (winning plan)
+// use before tuning to identify variables for indexing
+// use after to confirm indexes are used in the winning plan
+db.col.find(query).explain()
